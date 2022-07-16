6b8570cfc802cdecf6674296f3ecb894e9696f93
Madara0978 <109420589+Madara0978@users.noreply.github.com>
2022-07-16 14:47:06 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,19 +19,19 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['201032537866']
+global.premium = ['201032537866']
+global.ownernomer = '201032537866'
+global.ownername = 'madara'
+global.botname = 'Madara'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
-global.sessionName = 'session'
+global.packname = 'madara'
+global.author = 'madara'
+global.sessionName = 'madara'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
